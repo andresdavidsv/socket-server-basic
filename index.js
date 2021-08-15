2950f7c77d2ed121850ead62ab4e5f57fa733573
Andres David Solarte Vidal <andresdavidsolartevidal@gmail.com>
2021-08-15 16:34:31 -0500

    :sparkles: Send events to one or all clients

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ io.on('connection', (socket) => {
     msg: 'Hello, welcome to server',
     date: new Date()
   });
+  // socket.on('client-msg', (data) => {
+  //   console.log(data)
+  // })
+  socket.on('msg-to-server', (data) => {
+    console.log(data);
+    //All clients
+    io.emit('msg-from-server', data);
+    // Only client that dispatch one event
+    // socket.emit('msg-from-server', data);
+  })
 });
 server.listen(8080, () => {
   console.log('Server Listenend at Port 8080');

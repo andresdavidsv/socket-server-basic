480202604b5b6a7a8f040a99a379f90b21f4912b
Andres David Solarte Vidal <andresdavidsolartevidal@gmail.com>
2021-08-15 16:01:43 -0500

    :sparkles: Add event to send and recive

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const app = require('express')();
+const express = require('express')
+const app = express();
+
 const server = require('http').createServer(app);
+
 const io = require('socket.io')(server);
-io.on('connection', () => {
-  console.log('Client Connect');
+
+app.use(express.static(__dirname + '/public'))
+
+io.on('connection', (socket) => {
+  socket.emit('welcome', {
+    msg: 'Hello, welcome to server',
+    date: new Date()
+  });
 });
 server.listen(8080, () => {
   console.log('Server Listenend at Port 8080');

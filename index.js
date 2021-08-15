5fccab349f90ec2c1eb29750474ce844d93c2932
Andres David Solarte Vidal <andresdavidsolartevidal@gmail.com>
2021-08-15 16:50:56 -0500

    :recycle: Refat index.js of server. Conver to class

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-const express = require('express')
-const app = express();
+const Server = require('./models/server')
+require('dotenv').config();
+const server = new Server()
 
-const server = require('http').createServer(app);
-
-const io = require('socket.io')(server);
-
-app.use(express.static(__dirname + '/public'))
-
-io.on('connection', (socket) => {
-  socket.emit('welcome', {
-    msg: 'Hello, welcome to server',
-    date: new Date()
-  });
-  // socket.on('client-msg', (data) => {
-  //   console.log(data)
-  // })
-  socket.on('msg-to-server', (data) => {
-    console.log(data);
-    //All clients
-    io.emit('msg-from-server', data);
-    // Only client that dispatch one event
-    // socket.emit('msg-from-server', data);
-  })
-});
-server.listen(8080, () => {
-  console.log('Server Listenend at Port 8080');
-});
+server.execute();
